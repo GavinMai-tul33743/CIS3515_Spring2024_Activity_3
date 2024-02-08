33951e722e39f8ad8602fd85a6b354a3a5520c57
Gavin Mai <tul33743@temple.edu>
2024-02-08 11:49:42 -0500

    implemented getDropDownView, changed text colors

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,37 +1,51 @@
 package edu.temple.inclassuiacvitivity
 
 import android.content.Context
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class TextSizeAdapter(_context: Context, _spinner: View, _textSizes:Array<Int>) : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _spinner: Int, _textSizes:Array<Int>) : BaseAdapter() {
 
     private val context = _context
     private val spinner = _spinner
     private val textSizes = _textSizes
     override fun getCount(): Int {
-        TODO("Not yet implemented")
         return textSizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
         return textSizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
         return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
-        val textView = TextView(context)
+        val textView:TextView
+        if(convertView != null){
+            textView = convertView as TextView
+        }
+        else{
+            textView = TextView(context)
+        }
+
         textView.text = textSizes[position].toString()
         textView.textSize = textSizes[position].toFloat()
+        textView.setTextColor(Color.parseColor("Red"))
+        textView.setPadding(10, 5, 0, 5)
         return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return super.getDropDownView(position, convertView, parent).apply {
+            setBackgroundColor(Color.parseColor("Black"))
+        }
+
+
     }
 
 }
