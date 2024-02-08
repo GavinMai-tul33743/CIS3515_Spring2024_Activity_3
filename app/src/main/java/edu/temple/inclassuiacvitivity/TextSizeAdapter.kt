35db3624f32760f0633f23829b099527b550e0ad
Gavin Mai <tul33743@temple.edu>
2024-02-08 11:34:41 -0500

    implement getView, change adapter to custom adapter in main

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter(_context: Context, _textSizes:Array<Int>) : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _spinner: View, _textSizes:Array<Int>) : BaseAdapter() {
 
     private val context = _context
+    private val spinner = _spinner
     private val textSizes = _textSizes
     override fun getCount(): Int {
         TODO("Not yet implemented")
@@ -26,6 +28,10 @@ class TextSizeAdapter(_context: Context, _textSizes:Array<Int>) : BaseAdapter() 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = textSizes[position].toString()
+        textView.textSize = textSizes[position].toFloat()
+        return textView
     }
 
 }
